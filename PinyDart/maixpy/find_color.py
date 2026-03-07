6fa55862c9b037de5bb68cfae35e68040ac002ce
TAN_WSL <1240973173@qq.com>
2026-03-07 15:04:48 +0800

    <feat>(find_color)：add color threshold detecting with hsv, add roi detect and parse, add find contors and circles

--- PinyDart/maixpy/find_color.py
+++ PinyDart/maixpy/find_color.py
@@ -1,21 +1,84 @@
-from maix import camera, display, app, time
+from maix import camera, display, app, time, image
+import math
 
-cam = camera.Camera(512, 320)   # Manually set resolution
-                                # | 手动设置分辨率
-disp = display.Display()        # MaixCAM default is 522x368
-                                # | MaixCAM 默认是 522x368
-green_lab = [(0,80,-100,-10,0,30)]
+width = 512
+height = 320
+
+cam = camera.Camera(width, height)
+disp = display.Display()
+
+green_lab = [(88,92,-8,20,-17,0)]
+
+final_img = image.Image(width, int(height/2))
 
 while not app.need_exit():
-    # time.fps_start()          # Manually set fps calculation start point, comment here mean last time fps() call is start
-                                # | 动设置帧率（FPS）计算开始点，这里注释了表示上一次 fps 函数即是开始
-    img = cam.read()            # Get one frame from camera, img is maix.image.Image type object
-                                # | 从摄像头获取一帧图像，img 是 maix.image.Image 类型的对象
-    img.binary(green_lab)
-    disp.show(img)              # Show image to screen
-                                # | 将图像显示到屏幕
-    fps = time.fps()            # Calculate FPS between last time fps() call and this time call.
-                                # | 计算两次 fps 函数调用之间的帧率
-    print(f"time: {1000/fps:.02f}ms, fps: {fps:.02f}") # print FPS in console
-                                                       # | 在终端打印帧率（FPS）
 
+    img = cam.read()
+    final_img.clear()
+
+    bin_img = img.copy()
+    bin_img.binary(green_lab)
+
+    blobs = bin_img.find_blobs([(100,255)], pixels_threshold=5, area_threshold=5)
+
+    max_blob = None
+    max_area = 0
+
+    for b in blobs:
+
+        x, y, w, h = b.rect()
+        area = w * h
+
+        if area > max_area:
+            max_area = area
+            max_blob = b
+
+        img.draw_rect(x, y, w, h, image.COLOR_BLUE)
+        bin_img.draw_rect(x, y, w, h, image.COLOR_BLUE)
+
+    roi_img = None
+
+    # -------------------------
+    # 最大blob圆形检测
+    # -------------------------
+
+    if max_blob:
+
+        x, y, w, h = max_blob.rect()
+
+        pixels = max_blob.pixels()
+
+        circularity = pixels / (w * h)
+
+        is_circle = circularity > 0.6
+
+        if is_circle:
+
+            # 绿色框 = 圆形目标
+            img.draw_rect(x, y, w, h, image.COLOR_GREEN, 2)
+            bin_img.draw_rect(x, y, w, h, image.COLOR_GREEN, 2)
+
+            roi_img = img.crop(x, y, w, h)
+
+            roi_img.binary(green_lab)
+            roi_img.erode(1)
+            roi_img.dilate(1)
+
+            final_img.draw_image(0, height//3, roi_img)
+
+        else:
+
+            # 红色框 = 非圆形
+            img.draw_rect(x, y, w, h, image.COLOR_RED, 2)
+            bin_img.draw_rect(x, y, w, h, image.COLOR_RED, 2)
+
+    resize_img_raw = img.resize(width//3, height//3)
+    resize_img_par = bin_img.resize(width//2, height//2)
+
+    final_img.draw_image(0, 0, resize_img_raw)
+    final_img.draw_image(width//3, 0, resize_img_par)
+
+    disp.show(final_img)
+
+    fps = time.fps()
+    print(f"time: {1000/fps:.02f}ms, fps: {fps:.02f}")
